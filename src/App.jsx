7490c652dd8cd28f525bf585a0f1fe5e61021096
Dakota Gordon <dgordon@gibsonconnect.com>
2023-04-21 16:35:39 -0500

    Intro Section With Dropdown Commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import Nav from "./components/Nav";
+
+function App() {
+  return (
+    <>
+      <Nav />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Nav.jsx
@@ -0,0 +1,32 @@
+function Nav() {
+  return (
+    <>
+      <ul id="nav-links">
+        <li>
+          <a href="#">Features</a>
+        </li>
+        <li>
+          <a href="#">Company</a>
+        </li>
+        <li>
+          <a href="#">Careers</a>
+        </li>
+        <li>
+          <a href="#">About</a>
+        </li>
+        <div id="login-register">
+          <li>
+            <a href="#">Login</a>
+          </li>
+          <li>
+            <a href="#">
+              <button>Register</button>
+            </a>
+          </li>
+        </div>
+      </ul>
+    </>
+  );
+}
+
+export default Nav;
